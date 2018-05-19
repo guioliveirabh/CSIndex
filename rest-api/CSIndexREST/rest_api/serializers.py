eefe1437b27e45327f2e67dcd152f2a5d645c868
Guilherme Oliveira <guioliveirabh@users.noreply.github.com>
2018-05-19 11:44:04 -0300

    Adding Confereces in rest

--- rest-api/CSIndexREST/rest_api/serializers.py
+++ rest-api/CSIndexREST/rest_api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import Area
+from .models import Area, Conference
 
 
 class AreaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Area
         fields = ('id', 'name', 'label')
+
+
+class ConferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Conference
+        fields = ('id', 'area', 'name')
--- rest-api/CSIndexREST/rest_api/views.py
+++ rest-api/CSIndexREST/rest_api/views.py
@@ -1,9 +1,21 @@
 from rest_framework import viewsets
 
-from .models import Area
-from .serializers import AreaSerializer
+from .models import Area, Conference
+from .serializers import AreaSerializer, ConferenceSerializer
 
 
 class AreaViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Area.objects.all()
     serializer_class = AreaSerializer
+
+
+class ConferenceViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ConferenceSerializer
+    queryset = Conference.objects.all()
+
+    def get_queryset(self):
+        queryset = Conference.objects.all()
+        area_name = self.request.query_params.get('area', None)
+        if area_name:
+            queryset = queryset.filter(area__name=area_name)
+        return queryset
